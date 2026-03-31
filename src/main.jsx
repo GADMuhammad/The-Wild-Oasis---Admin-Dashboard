6c37a37a2e1ee0b5d6afe72b0a44b4310f79d29d
Muhammad Gad <muhammadgadwork@gmail.com>
2026-03-31 06:24:32 +0200

    start...

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./ui/ErrorFallback";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => window.location.replace("/")}
-    >
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
