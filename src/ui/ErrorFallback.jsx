bb8ebd50b3040b5418c2a255ec64b1740d9e87af
Muhammad Gad <muhammadgadwork@gmail.com>
2026-05-23 01:07:26 +0300

    react-error-boundary 🐜

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Heading from "./Heading";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +30,17 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+export default function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <StyledErrorFallback>
+      <Box>
+        <Heading as="h1">Something went wrong 🧐.</Heading>
+        <p>{error.message}</p>
+        <Button size="large" onClick={resetErrorBoundary}>
+          Try again.
+        </Button>
+      </Box>
+    </StyledErrorFallback>
+  );
+}
