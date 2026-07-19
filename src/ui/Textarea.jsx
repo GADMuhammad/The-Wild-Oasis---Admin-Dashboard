5231de1625a4d26ae2e0905b1a4edc653d585e36
Muhammad Gad <muhammadgadwork@gmail.com>
2026-07-19 21:19:23 +0300

    middle

--- src/ui/Textarea.jsx
+++ src/ui/Textarea.jsx
@@ -1,7 +1,7 @@
 export default function Textarea({ className = "", ...props }) {
   return (
     <textarea
-      className={`border-grey-300 bg-grey-0 h-32 w-xl resize-none rounded-sm border px-[1.2rem] py-[0.8rem] shadow-sm ${className}`}
+      className={`border-grey-300 bg-grey-0 h-32 w-lg resize-none rounded-sm border px-[1.2rem] py-[0.8rem] shadow-sm ${className}`}
       {...props}
     />
   );
