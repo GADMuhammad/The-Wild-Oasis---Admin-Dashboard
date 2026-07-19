b9124efe6ec9ba6c9833df8c6fed7cec6c71a067
Muhammad Gad <muhammadgadwork@gmail.com>
2026-07-19 21:19:20 +0300

    width

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,7 +1,7 @@
 export default function Input({ className = "", ...props }) {
   return (
     <input
-      className={`w-auto rounded-sm border border-grey-300 bg-grey-0 px-[1.2rem] py-[0.8rem] shadow-sm ${className}`}
+      className={`border-grey-300 bg-grey-0 w-xl rounded-sm border px-[1.2rem] py-[0.8rem] shadow-sm ${className}`}
       {...props}
     />
   );
--- src/ui/Textarea.jsx
+++ src/ui/Textarea.jsx
@@ -1,7 +1,7 @@
 export default function Textarea({ className = "", ...props }) {
   return (
     <textarea
-      className={`h-[8rem] w-full resize-none rounded-sm border border-grey-300 bg-grey-0 px-[1.2rem] py-[0.8rem] shadow-sm ${className}`}
+      className={`border-grey-300 bg-grey-0 h-32 w-xl resize-none rounded-sm border px-[1.2rem] py-[0.8rem] shadow-sm ${className}`}
       {...props}
     />
   );
